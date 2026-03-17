52eba090657a5e7e34933b76f153d5332a9302e6
Vaire Riiet <vaire481@gmail.com>
2026-03-17 20:55:28 +0200

    fixes

--- frontend/schemas/register-schema.ts
+++ frontend/schemas/register-schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const registerSchema = z.object({
-  email: z.string().email("Invalid email"),
+  email: z.string().trim().email("Invalid email"),
   password: z
     .string()
     .min(8, "Password must be at least 8 characters"),
