bf5f6c5d4023d730652b242c97a3b0bf25ca335c
laura <stinalaura.parg@gmail.com>
2026-04-19 17:29:30 +0300

    Update SecurityConfig to allow all requests instead of requiring authentication

--- backend/src/main/java/com/uihell/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/uihell/backend/config/SecurityConfig.java
@@ -99,7 +99,7 @@ public class SecurityConfig {
                     )
                     .permitAll()
                     .anyRequest()
-                    .authenticated()
+                    .permitAll()
             )
             //  Handle unauthorized access (no/invalid token)
             .exceptionHandling(ex ->
